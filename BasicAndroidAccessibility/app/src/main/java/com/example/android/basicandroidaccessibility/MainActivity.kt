ca1d0c36d7f80f002ad740441e6477f9dfd4a671
Murat Yener <yenerm@google.com>
2019-04-26 11:27:44 -0400

    Fixes
    UI Fixes, AppBar Navigation fixes, Label fixes, removing old activity declarations

--- BasicAndroidAccessibility/app/src/main/java/com/example/android/basicandroidaccessibility/MainActivity.kt
+++ BasicAndroidAccessibility/app/src/main/java/com/example/android/basicandroidaccessibility/MainActivity.kt
@@ -16,13 +16,33 @@
 
 package com.example.android.basicandroidaccessibility
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration : AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val host: NavHostFragment = supportFragmentManager
+                .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
+
+        // Set up Action Bar
+        val navController = host.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+
     }
 }
